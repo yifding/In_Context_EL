5cc99b10e3d603ff72b7973785fb6649e592193f
Yifan Ding <dyf0125@gmail.com>
2023-03-23 10:52:03 -0400

    add gpt3.5

--- in_context_el/openai_function.py
+++ in_context_el/openai_function.py
@@ -1,4 +1,5 @@
 import openai
+from tqdm import tqdm
 
 def openai_chatgpt(prompt, model="gpt-3.5-turbo"):
     openai_output = openai.ChatCompletion.create(
@@ -9,3 +10,21 @@ def openai_chatgpt(prompt, model="gpt-3.5-turbo"):
             )
     complete_output = openai_output["choices"][0]["message"]['content']
     return complete_output
+
+
+def openai_completion(prompt, model="text-davinci-003"):
+    openai_output = openai.Completion.create(
+        model=model,
+        prompt=prompt,
+    )
+    complete_output = openai_output["choices"][0]['text']
+    return complete_output
+
+
+if __name__ == '__main__':
+    from in_context_el.openai_key import OPENAI_API_KEY
+    prompt = 'it is just a test'
+    openai.api_key = OPENAI_API_KEY
+    for _ in tqdm(range(100)):
+        complete_output = openai_completion(prompt)
+    
